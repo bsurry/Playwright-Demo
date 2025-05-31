8aa054ae0920c7fdf2febe43b551ed8dd4733662
Beth Surry <b.surry@gmail.com>
2025-05-31 14:45:17 -0400

    need to actually generate allure report in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['dot'], 
                               ['html'],
-                              ['playwright-ctrf-json-reporter', {}]] : 
+                              ['playwright-ctrf-json-reporter', {}], ["allure-playwright"]] : 
                               [['list'], ['html'], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 40000,
